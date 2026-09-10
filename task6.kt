2b63d0a1c86e02e824b2690f86fe16da6328883a
Roman Khadraliev <Romch33tos@yandex.ru>
2026-09-10 08:48:59 +0700

    Задание 6 выполнено
    Реализована сортировка по частоте и алфавиту, а также вывод результата

--- task6.kt
+++ task6.kt
@@ -10,6 +10,15 @@ fun main(commandLineArguments: Array<String>) {
     .filter { word -> word.isNotEmpty() }
 
   val wordToOccurrenceCount = allWords.groupingBy { word -> word }.eachCount()
+
+  val entriesSortedByFrequencyThenAlphabet = wordToOccurrenceCount.entries.sortedWith(
+    compareByDescending<Map.Entry<String, Int>> { entry -> entry.value }
+      .thenBy { entry -> entry.key }
+  )
+
+  for ((word, occurrenceCount) in entriesSortedByFrequencyThenAlphabet) {
+    println("$word $occurrenceCount")
+  }
 }
 
 fun readFromStandardInput(): String {
